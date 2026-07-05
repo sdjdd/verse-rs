8b22a48494a04d1f3682c6630633cd57ffd00a38
sdjdd <i@sdjdd.com>
2026-07-05 13:43:19 +0800

    refactor(semantic): move binary_expr and type_expr implementations to main module

--- src/semantic/mod.rs
+++ src/semantic/mod.rs
@@ -12,9 +12,6 @@ use crate::{
 
 pub mod builtins;
 
-mod binary_expr;
-mod type_expr;
-
 #[derive(Hash, PartialEq, Eq, Clone, Debug)]
 pub enum TypeInfo {
     Void,
@@ -766,6 +763,89 @@ impl SemanticAnalyzer {
                 }
             }
         }
+    }
+
+    fn handle_binary_expr(&mut self, span: Span, expr: &BinaryExpr) -> AnalysisResult {
+        let lhs_ar = self.handle_expr(&expr.lhs);
+        let rhs_ar = self.handle_expr(&expr.rhs);
+        let lhs_type = lhs_ar.expr_type;
+        let rhs_type = rhs_ar.expr_type;
+
+        let type_id = if lhs_type == rhs_type {
+            lhs_type
+        } else {
+            self.emit_type_mismatch_error(span.clone(), lhs_type, rhs_type);
+            self.builtin_types.t_any
+        };
+
+        AnalysisResult {
+            expr_type: type_id,
+            ir_id: if let (Some(lhs_ir), Some(rhs_ir)) = (lhs_ar.ir_id, rhs_ar.ir_id) {
+                Some(self.emit_ir(
+                    ir::ExprKind::Binary(ir::BinaryExpr {
+                        lhs: lhs_ir,
+                        op: expr.op,
+                        rhs: rhs_ir,
+                    }),
+                    type_id,
+                ))
+            } else {
+                None
+            },
+        }
+    }
+
+    fn handle_type_expr(&mut self, expr: &TypeExpr) -> TypeId {
+        let type_id = match &expr.kind {
+            TypeExprKind::Named(symbol) => (|| -> TypeId {
+                if let Some(binding) = self.lookup(symbol) {
+                    let type_id = binding.type_id;
+                    if let Some(ty) = self.types.lookup(type_id) {
+                        if let TypeInfo::Type(inner_type) = ty {
+                            return *inner_type;
+                        } else {
+                            self.errors.push(SemanticError::UnexpectedExpr {
+                                span: expr.span.clone(),
+                                expect: "type".to_string(),
+                                found: "value".to_string(),
+                            });
+                            return self.builtin_types.t_any;
+                        }
+                    }
+                }
+                self.errors.push(SemanticError::TypeNotFound {
+                    span: expr.span.clone(),
+                    symbol: *symbol,
+                });
+                self.builtin_types.t_any
+            })(),
+            TypeExprKind::Option(inner) => {
+                let inner = self.handle_type_expr(inner);
+                self.types.intern(TypeInfo::Option(inner))
+            }
+            TypeExprKind::Tuple(args) => {
+                let mut arg_ids = vec![];
+                for arg in args {
+                    arg_ids.push(self.handle_type_expr(arg));
+                }
+                self.types.intern(TypeInfo::Tuple(arg_ids))
+            }
+            TypeExprKind::Function { params, ret } => {
+                let param_types: Vec<_> = params.iter().map(|p| self.handle_type_expr(p)).collect();
+                let ret_ty = self.handle_type_expr(ret);
+                let inner_type = self.types.intern(TypeInfo::Function {
+                    params: param_types,
+                    ret: ret_ty,
+                });
+                self.types.intern(TypeInfo::Type(inner_type))
+            }
+            TypeExprKind::Type => {
+                panic!("{:?}", expr);
+                // self.builtin_types.t_any
+            }
+        };
+
+        type_id
     }
 
     fn handle_member_expr(&mut self, expr: &MemberExpr) -> AnalysisResult {
